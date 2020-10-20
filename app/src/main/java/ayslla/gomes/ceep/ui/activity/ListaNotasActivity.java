96143a5bf140556f77e16e955eff693574b0c324
ayslla.gomes <04414927110@git.economia.gov.br>
2020-10-20 16:56:52 -0300

    Verificando a performace do projeto com ListView, para a adição de nota fixa

--- app/src/main/java/ayslla/gomes/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/ayslla/gomes/ceep/ui/activity/ListaNotasActivity.java
@@ -1,10 +1,16 @@
 package ayslla.gomes.ceep.ui.activity;
 
-import android.os.Bundle;
+import java.util.List;
 
-import ayslla.gomes.ceep.R;
+import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import ayslla.gomes.ceep.R;
+import ayslla.gomes.ceep.model.Nota;
+import ayslla.gomes.ceep.dao.NotaDAO;
+import ayslla.gomes.ceep.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,6 +18,14 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.lista_notas_listagem);
+
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira nota", "Primeira descrição"));
+        List<Nota> todasNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
     }
 
 }
